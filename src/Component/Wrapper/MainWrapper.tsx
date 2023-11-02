6157b5b9057929c8a00eea5eb64f9379f03ede5f
learnttuce0321 <lettucegood2@gmail.com>
2023-11-02 13:08:58 +0900

    feat: wrapper component

--- src/Component/Wrapper/MainWrapper.tsx
+++ src/Component/Wrapper/MainWrapper.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 
+// mainPage Wrapper
 const MainWrapper = styled.main`
     padding: 20px;
     height: calc(100vh - 3rem - 40px);
--- src/Component/Wrapper/OtherWrapper.tsx
+++ src/Component/Wrapper/OtherWrapper.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+// mainPage를 제외한 나머지 Page Wrapper
 const OtherWrapper = styled.main`
     height: calc(100vh - 5.5rem - 40.67px);
     padding: 20px;
