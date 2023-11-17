36da017ca81729639475b19b513ffff321f5a51b
learnttuce0321 <lettucegood2@gmail.com>
2023-11-17 10:50:51 +0900

    Fix: vercel Error

--- src/Component/navigation/SubNavigation/SubNavigation.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-// 보조 네비게이션 바
-const SubNavigation = styled.ul`
-    display: flex;
-    border-bottom: 2px solid black;
-    margin-bottom: 1rem;
-`
-export default SubNavigation
